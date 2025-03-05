4e2e25c17087d5c77e30472483121a94ec48ec89
Dhileepguru <dhileepguru497@gmail.com>
2025-03-05 11:51:29 +0530

    Modified Header Component

---
+++ React-App/src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header()
+{
+    return(
+        <div className="mt-5 flex flex-col gap-5 items-center justify-center text-white">
+            <h1 className="text-5xl font-bold">Your Ideal Job Awaits, Start the Search</h1>
+            <p className="text-xl">Get latest Job Openings that best suits you!!</p>
+        </div>
+    )
+}
+
+export default Header;
