e242322a1f18844e2b21f6013106379f41b2d36e
Dhileepguru <dhileepguru497@gmail.com>
2025-03-05 10:00:06 +0530

    Modified div

--- React-App/src/App.jsx
+++ React-App/src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 
+
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <h1 className='text-3xl font-bold underline'>Hello React and Tailwind</h1>
-      </div>
+      <Navbar />
       
     </>
   )
---
+++ React-App/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+
+function Navbar()
+{
+    return (
+    <div className=".nav">
+        <h1 className='text-3xl font-bold mt-2 ml-2 font-serif'>JobNest</h1>
+      </div>
+    )
+}
+
+export default Navbar;
