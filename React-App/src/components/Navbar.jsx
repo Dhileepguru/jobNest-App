99ba4e5b916b491a89ada14f1b0a77171d885af3
Dhileepguru <dhileepguru497@gmail.com>
2025-03-05 11:48:12 +0530

    Modified Navbar

--- React-App/src/components/Navbar.jsx
+++ React-App/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 
-function Navbar()
+function NavBar()
 {
     return (
-    <div className=".nav">
-        <h1 className='text-3xl font-bold mt-2 ml-2 font-serif'>JobNest</h1>
+    <div className="mt-2 h-20 flex w-full text-white ">
+        <h1 className='text-3xl font-bold pl-2  ml-2'>JobNest.</h1>
       </div>
     )
 }
 
-export default Navbar;
+export default NavBar;
