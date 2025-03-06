c3a14feed5b55da07225151dcc27b38ab10d0ad2
Dhileepguru <dhileepguru497@gmail.com>
2025-03-06 09:48:08 +0530

    Dynamically Created Job Datas

---
+++ React-App/src/components/JobDummyData.js
@@ -0,0 +1,36 @@
+export default [
+        {
+            id:1,
+            postedOn:"2025-3-3",
+            title:'Front end Developer',
+            company:'Amazon',
+            type:'Full Time',
+            experience: 'Fresher',
+            location: 'In-Office',
+            skills: ["javaScript","node js", "react js"],
+            jobLinks:"https://github.com/Dhileepguru/jobNest-App"
+        },
+        {
+            id:2,
+            postedOn: "2025-3-3",
+            title:'Back end Developer',
+            company:'Amazon',
+            type:'Full Time',
+            experience: 'Fresher',
+            location: 'In-Office',
+            skills: ["javaScript","node js", "react js"],
+            jobLinks:"https://github.com/Dhileepguru/jobNest-App"
+        },
+        {
+            id:3,
+            postedOn: "2025-3-3",
+            title:'Android Developer',
+            company:'Amazon',
+            type:'Full Time',
+            experience: 'Fresher',
+            location: 'In-Office',
+            skills: ["javaScript","node js", "react js"],
+            jobLinks:"https://github.com/Dhileepguru/jobNest-App"
+        },
+        
+]
