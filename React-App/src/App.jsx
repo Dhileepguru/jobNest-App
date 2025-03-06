0df5fc88b1028285afe5d0f23b4608096d73afc0
Dhileepguru <dhileepguru497@gmail.com>
2025-03-06 11:22:42 +0530

    Fetched Firebase Datas

--- React-App/src/App.jsx
+++ React-App/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 
 import './App.css'
@@ -8,15 +8,35 @@ import SearchBar from './components/SearchBar'
 import JobCard from './components/JobCard'
 import JobData from './components/JobDummyData'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+import { collection, query, where, getDocs } from "firebase/firestore"; 
+import { db } from './firebase.config.js'
+
+function App() {
+  const [jobs,setJobs]=useState([]);
+  const fetchData = async()=>
+  {
+    const tempJobs=[];
+    const q = query(collection(db, "jobs"));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((job) => {
+     tempJobs.push({...job.data(),
+      id:job.id
+     })
+    });
+    setJobs(tempJobs);
+  }
+  useEffect(()=>
+  {
+    fetchData();
+  },[])
+  
   return (
     <>
       <NavBar />
       <Header />
       <SearchBar />
-      {JobData.map((job)=> // title={job.title}  company={job.company} type={job.type} experience={job.experience} location={job.location} skills={job.skills} jobLinks={job.jobLinks}
+      {jobs.map((job)=> // title={job.title}  company={job.company} type={job.type} experience={job.experience} location={job.location} skills={job.skills} jobLinks={job.jobLinks}
         <JobCard key={job.id} {...job}/>//spread operator
       )}
     </>
