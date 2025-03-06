2787a004557697cde443e20a2dd31753452c526a
Dhileepguru <dhileepguru497@gmail.com>
2025-03-06 20:50:37 +0530

    Updated Query to Solve Filter Issue

--- React-App/src/App.jsx
+++ React-App/src/App.jsx
@@ -7,7 +7,7 @@ import JobCard from './components/JobCard'
 import JobData from './components/JobDummyData'
 
 
-import { collection, query, where, getDocs } from "firebase/firestore"; 
+import { collection, query, where, getDocs, orderBy } from "firebase/firestore"; 
 import { db } from './firebase.config.js'
 
 function App() {
@@ -25,40 +25,56 @@ function App() {
     });
     setJobs(tempJobs);
   }
+
   useEffect(()=>
   {
     fetchData();
   },[])
 
-  const fetchJobsCustom = async (jobCriteria) => {
-    try {
-      let jobsRef = collection(db, "jobs");
-      let q = jobsRef;
-      if (jobCriteria.title) q = query(q, where("title", "==", jobCriteria.title));
-      if (jobCriteria.type) q = query(q, where("type", "==", jobCriteria.type));
-      if (jobCriteria.location) q = query(q, where("location", "==", jobCriteria.location));
-      if (jobCriteria.experience) q = query(q, where("experience", "==", jobCriteria.experience));
-  
-      const querySnapshot = await getDocs(q);
-      const tempJobs = querySnapshot.docs.map((job) => ({
-        ...job.data(),
-        id: job.id,
-        postedOn: job.data().postedOn.toDate(),
-      }));
-  
-      setJobs(tempJobs);
-    } catch (error) {
-      console.error("Error fetching jobs:", error);
+  const CustomFetchData = async (jobCriteria) => {
+    console.log("Fetching jobs with criteria:", jobCriteria);
+
+    const tempJobs = [];
+    const jobsRef = collection(db, "jobs");
+
+    // Construct the query dynamically
+    let q = query(jobsRef);
+
+    if (jobCriteria.type) {
+        q = query(q, where("type", "==", jobCriteria.type));
     }
-  };
-  
-  
-  
+    if (jobCriteria.experience) {
+        q = query(q, where("experience", "==", jobCriteria.experience));
+    }
+    if (jobCriteria.location) {
+        q = query(q, where("location", "==", jobCriteria.location));
+    }
+    if (jobCriteria.title) {
+        q = query(q, where("title", "==", jobCriteria.title));
+    }
+
+    console.log("Final Query: ", q);
+
+    const req = await getDocs(q);
+    console.log("Query Snapshot: ", req.empty ? "No results found" : req.docs.length);
+
+    req.forEach((job) => {
+        tempJobs.push({
+            ...job.data(),
+            id: job.id,
+            postedOn: job.data().postedOn.toDate(),
+        });
+    });
+
+    setJobs(tempJobs);
+};
+
+
   return (
     <>
       <NavBar />
       <Header />
-      <SearchBar  fetchJobsCustom={fetchJobsCustom}/>
+      <SearchBar CustomFetchData={CustomFetchData} />
       {jobs.map((job)=> // title={job.title}  company={job.company} type={job.type} experience={job.experience} location={job.location} skills={job.skills} jobLinks={job.jobLinks}
         <JobCard key={job.id} {...job}/>//spread operator
       )}
