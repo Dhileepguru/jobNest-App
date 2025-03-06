2b70218ec6b0b27ea12cbf595fa7d0ec1605ac78
Dhileepguru <dhileepguru497@gmail.com>
2025-03-06 09:45:36 +0530

    Inserted Props elements

--- React-App/src/App.jsx
+++ React-App/src/App.jsx
@@ -6,6 +6,7 @@ import NavBar from './components/Navbar'
 import Header from './components/Header'
 import SearchBar from './components/SearchBar'
 import JobCard from './components/JobCard'
+import JobData from './components/JobDummyData'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,7 +16,9 @@ function App() {
       <NavBar />
       <Header />
       <SearchBar />
-      <JobCard />
+      {JobData.map((job)=> // title={job.title}  company={job.company} type={job.type} experience={job.experience} location={job.location} skills={job.skills} jobLinks={job.jobLinks}
+        <JobCard key={job.id} {...job}/>//spread operator
+      )}
     </>
   )
 }
