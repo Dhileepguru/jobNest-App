c12912fe31dd2dd6be70d86a7164613b7b36c608
Dhileepguru <dhileepguru497@gmail.com>
2025-03-06 09:46:12 +0530

    Called Props element to Jobcard

--- React-App/src/components/JobCard.jsx
+++ React-App/src/components/JobCard.jsx
@@ -1,26 +1,26 @@
 import dayjs from'dayjs';
-function JobCard()
-{
+function JobCard(props)
+{   
     let arraySkills=["Javascript","React js" ,"Nodejs"];
     let Date1 = dayjs(Date.now());
-    let dateDiff= Date1.diff('2025-3-4','day');
-
+    let dateDiff= Date1.diff(dayjs(props.postedOn),'day');
+    
 
         return(
-        <div className= "bg-white mx-40 mb-4 rounded-md  border-amber-200">
-            <div className='flex justify-between items-center px-6 py-4 border-black shadow-lg  hover:border-blue-500 hover:translate-y-1 hover:scale-103 '>
+        <div className= " hover:border-blue-500 hover:translate-y-1 hover:scale-103 bg-white mx-40 mb-4 rounded-md  border-amber-200">
+            <div className='flex justify-between items-center px-6 py-4 border-black shadow-lg  '>
              <div className='flex flex-col items-start gap-3'>
-                <h1>Frontend Developer - Amazon</h1>
-                <p>Full Time - Fresher - In-Office</p>
-                 <div>
-                    {arraySkills.map((ele)=>
-                        <p>{ele}</p>
+                <h1 className='text-lg font-semibold'>{props.title}</h1>
+                <p>{props.type} - {props.experience} - {props.location}</p>
+                 <div className='flex items-center gap-2'>
+                    {props.skills.map((ele)=>
+                        <p key={ele} className='text-gray-500 py-1 px-2 border rounded-md border-black'>{ele}</p>
                     )}
                  </div>
                  </div>
-                 <div>
-                   <p>Posted {dateDiff}</p>
-                    <button className=" bg-gray-700 text-white">Apply</button>
+                 <div className='flex items-center gap-4'>
+                   <p className='text-gray-500 mt-5'>Posted {dateDiff} Days Ago</p>
+                    <button href={props.jobLinks} className="text-blue-500 mt-4 border rounded-md py-2 px-10">Apply</button>
                 </div>
             </div>
         </div>
